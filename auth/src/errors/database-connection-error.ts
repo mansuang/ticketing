40f21f8a21aaf22077295f9732ba2e328d73c938
Mansuang Pawong <mansaung@hotmnail.com>
2022-09-28 21:36:14 +0700

    checkpoint

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,11 +1,19 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class DatabaseConnectionError extends Error {
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
     reason = 'Error connecting to database';
     constructor(public errors: ValidationError[]) {
-        super();
+        super('Error connecting to db');
 
         // Only because we are extending a build in class
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors() {
+        return [
+            { message: this.reason }
+        ]
     }
 }
