00338e78e8ae6ddd2396b99e6e4818d3528b48cd
Mansuang Pawong <mansaung@hotmnail.com>
2022-09-29 22:14:21 +0700

    add secret

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,9 +9,15 @@ import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}));
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,6 +31,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined')
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo:27017/auth');
         console.log('Connected to MongoDB')
