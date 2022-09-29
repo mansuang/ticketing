ede0ff875bbaf282ae5db21ecab71c0b34cb8a28
Mansuang Pawong <mansaung@hotmnail.com>
2022-09-29 18:13:05 +0700

    bad request error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -9,7 +11,7 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must between 4 and 20 chars'),
 
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -19,10 +21,17 @@ router.post('/api/users/signup', [
 
     const { email, password } = req.body;
 
-    console.log('Creating a user...', req.body);
-    throw new DatabaseConnectionError(errors.array());
+    const existingUser = await User.findOne({ email });
 
-    res.send({ message: 'signup' });
+    if (existingUser) {
+        throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
+
 });
 
 export { router as signupRouter };
