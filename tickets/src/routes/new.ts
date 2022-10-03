65f3db29a4338257371db9e22d60e01fc2f1ac0e
Mansuang Pawong <mansaung@hotmnail.com>
2022-10-03 19:34:28 +0700

    input validation

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from 'express'
-import { requireAuth } from '@thundertickets/common';
+import { requireAuth, validateRequest } from '@thundertickets/common';
+import { body } from 'express-validator'
+
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-    res.sendStatus(200);
-});
+router.post('/api/tickets',
+    requireAuth, [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than zero'),
+],
+    validateRequest, (req: Request, res: Response) => {
+
+
+    });
 
 export { router as createTicketRouter };
