4612c7e90c4b2dd799504d9df490bb4994f4a834
Mansuang Pawong <mansaung@hotmnail.com>
2022-10-03 15:52:57 +0700

    move common to npm

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@thundertickets/common';
 
 const router = express.Router();
 
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator';
-import { BadRequestError } from '../errors/bad-request-error';
-import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError, validateRequest } from '@thundertickets/common';
+
 import { User } from '../models/user';
 import { Password } from '../services/password';
 import jwt from 'jsonwebtoken'
