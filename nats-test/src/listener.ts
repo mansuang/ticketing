34a68747fb479ad75056be0935720c4a55640c3b
Mansuang Pawong <mansaung@hotmnail.com>
2022-10-13 23:33:15 +0700

    add on close and disconnect client

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,41 @@
-import nats from 'node-nats-streaming'
+import { randomBytes } from 'crypto';
+import nats, { Message } from 'node-nats-streaming'
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
     console.log('Listener connected to NATS');
 
-    const ticketCreatedChannel = stan.subscribe('ticket:created');
+    stan.on('close', () => {
+        console.log('NATS connection closed');
+        process.exit();
+    });
 
-    ticketCreatedChannel.on('message', (msg) => {
-        console.log('Message received')
+    const options = stan.subscriptionOptions()
+        .setManualAckMode(true)
+        .setDeliverAllAvailable()
+        .setDurableName('orders-service');
+
+    const ticketCreatedChannel = stan.subscribe(
+        'ticket:created',
+        'order-service-queue-group',
+        options
+    );
+
+    ticketCreatedChannel.on('message', (msg: Message) => {
+        console.log('Message received', {
+            time: new Date(),
+            sequence: msg.getSequence(),
+            data: msg.getData()
+        });
+
+        msg.ack();
     });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
