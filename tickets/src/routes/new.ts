633d8ff27b400e095af1868c1efaa57ff5afb544
Mansuang Pawong <mansaung@hotmnail.com>
2022-10-15 21:35:24 +0700

    update common event

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import { requireAuth, validateRequest } from '@thundertickets/common';
 import { body } from 'express-validator'
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../tickets/publishers/ticket-created-publisher';
 
 const router = express.Router();
 
@@ -23,6 +24,13 @@ router.post('/api/tickets',
         });
 
         await ticket.save();
+
+        await new TicketCreatedPublisher(client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId
+        });
 
         res.status(201).send(ticket);
     });
