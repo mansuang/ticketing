4555d3f2f79e8fbce9b337c69a8d914c145f184d
Mansuang Pawong <mansaung@hotmnail.com>
2022-10-26 21:57:55 +0700

    update client

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Tickets</h1>
+        <h1>Ticketsx</h1>
         <b-table striped hover :items="formatTickets" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc">
             <!-- A custom formatted column -->
             <template #cell(title)="data">
